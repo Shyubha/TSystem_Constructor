925df44dfc7d8df685ee4eb598516aae979aa160
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-05 13:42:44 +0530

    Add project files.

---
+++ Department.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_Constructor
+{
+    public class Department
+    {
+        private int id;
+        private string name, location;
+        public Department() //constructor
+        {
+            id = 1;
+            name = "Admin";
+            location = "Nashik";
+
+
+        }
+        public Department(int d_id, string d_name, string d_location)//parameterized constructor
+        {
+            id = d_id;
+            name = d_name;
+            location = d_location;
+
+
+        }
+        public string GetData()
+        {
+            return " Department Id " + id + "Department name" + name + "Location" + location;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿sing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department dept = new Department();
+            string str = dept.GetData();
+            Console.WriteLine(str);
+            Department dept2 = new Department(2, "civil", "pune");
+            String str2 = dept2.GetData();
+            Console.WriteLine(str2);
+
+
+
+
+
+            Student stud = new Student();
+            string str3 = stud.GetData();
+            Console.WriteLine(str3);
+            Student stud2 = new Student(102, "Piyush", "10th", "Thane");
+            string str4 = stud2.GetData();
+            Console.WriteLine(str4);
+            Console.ReadLine();
+
+        }
+    }
+}
